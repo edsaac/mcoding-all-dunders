fe8263afe8fead554f22e21d3a762005cf266bfd
Ed Saa <esaavedrac@u.northwestern.edu>
2023-07-21 20:40:35 -0500

    not really a dunder here yet

--- src/callable-cove/defaults.py
+++ src/callable-cove/defaults.py
@@ -1,27 +1,55 @@
-def str_to_list(string: str, header: list = [], repeat: int = 1) -> list:
-    """Naïve function to convert a string into a list of characters"""
-    for i in range(repeat):
-        for i in string:
-            header.append(i)
+from io import StringIO
 
-    return header
+
+def markdown_to_html(text: str, template={"#": "<h1>", "##": "<h2>"}) -> list:
+    html = StringIO()
+
+    for line in text.splitlines():
+        if "# " in line:
+            html.write("<h1>")
+            html.write(template[0] + line.replace("# ", "") + template[-1])
+            html.write("</h1>")
+
+        elif "## " in line:
+            html.write(line)
+
+    return html.getvalue()
 
 
 def main():
-    # Get a tuple with the function's default values
-    print(f"{str_to_list.__defaults__=}")
+    print(markdown_to_html("# A title"))
+    print(markdown_to_html("A paragraph"))
+    print(markdown_to_html("# A title"))
+    print(markdown_to_html("# A title"))
+    print(markdown_to_html("A paragraph"))
 
-    # Looks like it's working correctly!
-    print(f"{str_to_list('hello')=}")
 
-    # But the default is a mutable object :S
-    print(f"{str_to_list('HOLA')=}")
+# def str_to_list(string: str, header: list = [], repeat: int = 1) -> list:
+#     """Naïve function to convert a string into a list of characters"""
+#     for i in range(repeat):
+#         for i in string:
+#             header.append(i)
 
-    # The default value changed by calling the function :/
-    print(f"{str_to_list.__defaults__=}")
+#     return header
 
-    # Not the best implementation
-    print(f"{str_to_list('HOLA', header=[])=}")
+# def main():
+#     # Get a tuple with the function's default values
+#     print(f"{str_to_list.__defaults__=}")
+
+#     # Looks like it's working correctly!
+#     print(f"{str_to_list('hello')=}")
+
+#     # Looks like it's working correctly!
+#     print(f"{str_to_list('hello', header=['>'])=}")
+
+#     # But the default is a mutable object :S
+#     print(f"{str_to_list('HOLA')=}")
+
+#     # The default value changed by calling the function :/
+#     print(f"{str_to_list.__defaults__=}")
+
+#     # Not the best implementation
+#     print(f"{str_to_list('HOLA', header=[])=}")
 
 
 if __name__ == "__main__":
--- src/callable-cove/polygon_area.py
+++ src/callable-cove/polygon_area.py
@@ -2,7 +2,9 @@ from itertools import starmap
 from operator import mul
 
 
-def polygon_area(vertices: list[tuple[float, float]]) -> float:
+def polygon_area(
+    vertices: list[tuple[float, float]], *, scale: float = 1.0
+) -> float:
     """Calculate the area of a polygon from a list of
     coordinates x,y using the Gauss's (shoelace) formula.
 
@@ -20,8 +22,43 @@ def polygon_area(vertices: list[tuple[float, float]]) -> float:
     else:
         area = 0.0
 
-    return area
+    return area * scale**2
 
 
-print(f"{polygon_area([(0,0), (2,0), (2,2), (0,2)]) = }")
-print(f"{polygon_area([(0,0), (1,0), (0,1)]) = }")
+def batch_areas(*polygons):
+    return [polygon_area(p) for p in polygons]
+
+
+def main():
+    square = [(0, 0), (2, 0), (2, 2), (0, 2)]
+    triangle = [(0, 0), (1, 0), (0, 1)]
+    octagon = [
+        ( 0.414, 1.000),
+        ( 1.000, 0.414),
+        ( 1.000, -0.414),
+        ( 0.414, -1.000),
+        (-0.414, -1.000),
+        (-1.000, -0.414),
+        (-1.000, 0.414),
+        (-0.414, 1.000)
+    ]
+
+    print(f"{polygon_area(square) = }")
+    print(f"{polygon_area(triangle) = }")
+    print(f"{polygon_area(octagon) = }")
+
+    print(f"{polygon_area(square, scale=0.5) = }")
+
+    print(polygon_area.__defaults__)
+    print(polygon_area.__kwdefaults__)
+
+    print(
+        """
+        Turns out the coordinates are in m but we needed
+        the areas in ft²  
+        """
+    )
+
+
+if __name__ == "__main__":
+    main()
